e47458bbe8266f9e8bfd4e4fde6973c198ab4e0d
Theocharis Panagiotis Charalampidis <runtheorun3@gmail.com>
2022-08-16 16:29:19 +0300

    preprocessing script updated; NaNs dropped; Code Refined

--- Code/preprocessing.py
+++ Code/preprocessing.py
@@ -4,26 +4,38 @@ import datetime
 pd.set_option('display.max_columns', None)
 
 # Section cleans up datasets, dropping NaNs and converting dates to epoch time
-openweather = pd.read_csv('Sources/openweather.csv', sep = ';', header = 0)
-for index, row in openweather.iterrows():
-  if index != 0 :    
-    openweather[0][index] = time.mktime(datetime.datetime.strptime(openweather[0][index], "%Y-%m-%d %H:%M").timetuple())
-print (openweather[0][0])
-openweather.to_csv('Products/openweather2.csv', sep = ';', header = None, index = False)
+# openweather = pd.read_csv('Sources/openweather.csv', sep = ';', header = 0)
+# dict = list(openweather.columns)
+# dict[0] = "timestamp"
+# for index, row in openweather.iterrows():
+#   val = openweather.at[index,"dt_iso.1"]
+#   openweather.at[index,"dt_iso.1"] = time.mktime(datetime.datetime.strptime(val, "%Y-%m-%d %H:%M").timetuple())
+# print (openweather.at[6654,"dt_iso.1"]) #sample to check if conversion worked
+# openweather.to_csv('Products/openweather2.csv', sep = ';', header = dict, index=False)
 
-laertis = pd.read_csv('Sources/Laertis2.csv', sep = ';', header = 0)
-laertis = laertis.dropna()
-for index, row in laertis.iterrows():
-  if index != 0 :
-    laertis[0][index] = time.mktime(datetime.datetime.strptime(laertis[0][index], "%Y-%m-%d %H:%M").timetuple())
+# laertis = pd.read_csv('Sources/Laertis2.csv', sep = ';', header = 0)
+# dict = ["Laertis_timestamp","Laertis_humidity","Laertis_temperature","Laertis_pressure","Laertis_eCO2","Laertis_TVOC","Laertis_light","Laertis_PM1","Laertis_dBA","Laertis_PM10","Laertis_PM2.5"]
 
-laertis.to_csv('Products/laertisNew.csv', sep = ';', header = None, index = False)
+# laertis = laertis.dropna()
+# for index, row in laertis.iterrows():
+#   val = laertis.at[index,"timestamp.1"]
+#   laertis.at[index,"timestamp.1"] = time.mktime(datetime.datetime.strptime(val, "%Y-%m-%d %H:%M").timetuple())
+# print (laertis.at[6654,"timestamp.1"])
+# laertis.to_csv('Products/laertisNew.csv', sep = ';', header = dict, index=False)
 
 openweather = pd.read_csv('Products/openweather2.csv', sep = ';', header = 0)
 laertis = pd.read_csv('Products/laertisNew.csv', sep = ';', header = 0)
 
+dict = list(laertis.columns) + list(openweather.columns)
+dict.pop(11)
+print(dict)
+# Section merges datasets
 dataset = pd.merge(laertis,openweather,how='outer',on="timestamp")
 dataset = dataset.interpolate(method='cubic', axis=0)
+
 print(dataset.head(1))
 
-dataset.to_csv('Products/dataset.csv', sep = ';', header = None, index = False)
+dataset = dataset[dataset['weather_id'].notna()]
+dataset = dataset[dataset['Laertis_humidity'].notna()]
+dataset.to_csv('Products/dataset.csv', sep = ';', header = dict, index = False)
+# weather_id,'''
---
+++ Code/training.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+source = pd.read_csv('Products/dataset.csv', sep = ';', header = 0)
